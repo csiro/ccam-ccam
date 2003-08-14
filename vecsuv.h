47ac9e1bb196e9215e43a0d5ce93baebe72799fd
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-08-14 01:18:36 +0000

    Add declarations.

--- vecsuv.h
+++ vecsuv.h
@@ -1,6 +1,7 @@
-      common/vecsuv/ax(ifull),bx(ifull),ay(ifull),by(ifull)   ! re-ordered
-     .             ,az(ifull),bz(ifull)    !  ,cosa(ifull)    ! for globpea
-      dimension ax6(il,il,0:5),ay6(il,il,0:5),az6(il,il,0:5)
-     .         ,bx6(il,il,0:5),by6(il,il,0:5),bz6(il,il,0:5)
-      equivalence (ax6,ax),(ay6,ay),(az6,az)
-     .           ,(bx6,bx),(by6,by),(bz6,bz)
+      real ax, bx, ay, by, az, bz
+      common/vecsuv/ax(ifull),bx(ifull),ay(ifull),by(ifull),  ! re-ordered
+     &              az(ifull),bz(ifull)    !  ,cosa(ifull)    ! for globpea
+      real ax6(il,il,0:5),ay6(il,il,0:5),az6(il,il,0:5),
+     &     bx6(il,il,0:5),by6(il,il,0:5),bz6(il,il,0:5)
+      equivalence (ax6,ax),(ay6,ay),(az6,az),
+     &            (bx6,bx),(by6,by),(bz6,bz)
--- xyzinfo.h
+++ xyzinfo.h
@@ -1,4 +1,5 @@
+      real x, y, z, wts
       common/xyz/x(ifull),y(ifull),z(ifull),wts(ifull) ! rlat and rlong gone
-      dimension x6(il,il,0:5),y6(il,il,0:5),z6(il,il,0:5)
-      dimension x06(il,il,0:5),y06(il,il,0:5),z06(il,il,0:5)
+      real x6(il,il,0:5),y6(il,il,0:5),z6(il,il,0:5)
+      real x06(il,il,0:5),y06(il,il,0:5),z06(il,il,0:5)
       equivalence (x,x6,x06),(y,y6,y06),(z,z6,z06)
