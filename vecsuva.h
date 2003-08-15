59dfb054dd11220a63f54d49692e50ad5e9167ef
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-08-15 04:46:09 +0000

    Add declarations.

--- vecsuva.h
+++ vecsuva.h
@@ -1,2 +1,3 @@
+      real axu,bxv,ayu,byv,azu,bzv
       common/vecsuva/axu(ifull),bxv(ifull),ayu(ifull),byv(ifull),
-     .               azu(ifull),bzv(ifull)
+     &               azu(ifull),bzv(ifull)
