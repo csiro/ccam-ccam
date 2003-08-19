d8013ac76912bc3d4c4abe930bcd07ca727bdb57
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-08-19 01:14:03 +0000

    Combine constant.h with const_phys.h.

--- const_phys.h
+++ const_phys.h
@@ -1,30 +1,52 @@
 c Imported from CSIRO GCM
-      real stefbo, erad, eradsq, cp, rdry, epsil, rvap, hlf, hl
-     &, hls, hlcp, grav, hlfcp, hlscp, sq2, cappa, tomg, pi
 
-      parameter (stefbo=5.67e-8) !Stefan-Boltzmann constant
-      parameter (erad=6.37122e6, eradsq=erad*erad) !Radius of earth
-      parameter (cp=1004.64) ! Specific heat of dry air at const P
-      parameter (rdry=287.04) ! Specific gas const for dry air
-      parameter (epsil=0.622) ! Ratio molec wt of H2O vapour to dry air
-      parameter (rvap=461.) ! Gas constant for water vapour
-      parameter (hlf=3.35e5) ! Latent heat of fusion (at 0 deg C)
-      parameter (hl=2.5e6) !Latent heat of vaporization (at 0 deg. C)
-      parameter (hls=hl+hlf) !  "      "   " sublimation
-      parameter (hlcp=hl/cp, hlfcp=hlf/cp, hlscp=hlcp+hlfcp)
-      parameter (grav=9.80616) ! Acceleration of gravity
+      real, parameter :: stefbo=5.67e-8 !Stefan-Boltzmann constant
+      real, parameter :: erad=6.37122e6, eradsq=erad*erad !Radius of earth
+      real, parameter :: cp=1004.64 ! Specific heat of dry air at const P
+      real, parameter :: rdry=287.04 ! Specific gas const for dry air
+      real, parameter :: epsil=0.622 ! Ratio molec wt of H2O vapour to dry air
+      real, parameter :: rvap=461. ! Gas constant for water vapour
+!      real, parameter :: hlf=3.35e5 ! Latent heat of fusion (at 0 deg C)
+!     CC model value
+      real, parameter :: hlf=3.36e5 ! Latent heat of fusion (at 0 deg C)
+!      real, parameter :: hl=2.5e6 !Latent heat of vaporization (at 0 deg. C)
+!     CC model value.
+      real, parameter :: hl=2.5104e6 !Latent heat of vaporization (at 0 deg. C)
+      real, parameter :: hls=hl+hlf !  "      "   " sublimation
+      real, parameter :: hlcp=hl/cp, hlfcp=hlf/cp, hlscp=hlcp+hlfcp
+      real, parameter :: grav=9.80616 ! Acceleration of gravity
 
-      parameter (sq2=1.414213562373092) ! Square root of 2
-      parameter (cappa=rdry/cp) 
-      parameter (tomg=2*7.2921233e-5) ! 2*omega
-      parameter (pi=3.14159265) !Good ol' pi
+      real, parameter :: sq2=1.414213562373092 ! Square root of 2
+      real, parameter :: cappa=rdry/cp 
+      real, parameter :: tomg=2*7.2921233e-5 ! 2*omega
+      real, parameter :: pi=3.141592653589793, tpi=2.0*pi !Good ol' pi
 
-      real hcap50 ! Heat capacity of sea water * 50m (J/m**2/K)
-      real dzmlo  ! Depth of mixed layer ocean points
-      parameter (hcap50=2.095e8,dzmlo=100.0)
-      real hcap   ! Heat capacity of 1m of sea water (J/m**2/K)
-      parameter (hcap=hcap50/50.0)
+      real, parameter :: hcap50=2.095e8 ! Heat capacity of sea water * 50m (J/m**2/K)
+      real, parameter :: hdzmlo=100.0   ! Depth of mixed layer ocean points
+      real, parameter :: hcap=hcap50/50.0 ! Heat capacity of 1m of sea water (J/m**2/K)
 
-      real tfrz
-      parameter (tfrz=273.15)
+!      real, parameter :: tfrz=273.15
+!     CCAM value, Should this be 273.15 or is there a physical difference?
+      real, parameter :: tfrz=273.1
 
+      real, parameter :: cpv=1869.46 ! Specific heat of water vapor at const P
+      real, parameter :: roncp=cappa ! Just an alias for the CC model
+      real, parameter :: rearth=erad 
+      real, parameter :: ars=rvap
+      real, parameter :: hlars=hl/ars
+      real, parameter :: stdlapse=6.5e-3
+
+! --- chemical
+      real, parameter :: fAIR_MolM = 28.965,
+     &           fH_MolM   =  1.00794,
+     &           fC_MolM   = 12.011,
+     &           fO_MolM   = 15.9994,
+     &           fS_MolM   = 32.066,
+
+     &           fH2_MolM  = 2*fH_MolM,			! molecular hydrogen
+     &           fO2_MolM  = 2*fO_MolM,			! molecular oxygen
+     &           fCO2_MolM =   fC_MolM   +   fO2_MolM,	! carbon dioxide
+     &           fSO2_MolM =   fS_MolM   +   fO2_MolM,	! sulphur dioxide
+     &           fSO4_MolM =   fSO2_MolM +   fO2_MolM,	! sulphate
+     &           fCH4_MolM =   fC_MolM   + 2*fH2_MolM,	! methane
+     &           fH2O_MolM =   fO_MolM   +   fH2_MolM	! water
