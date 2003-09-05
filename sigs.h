61cae76f85961615dd6c9a09e4665f4385e25e3b
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-09-05 02:51:10 +0000

    Make continuations free format compatible.

--- sigs.h
+++ sigs.h
@@ -1,5 +1,5 @@
-      common/sigs/sig(kl),sigmh(kl),dsig(kl),rata(kl),ratb(kl),bet(kl)
-     . ,tbar(kl),tbardsig(kl),dtmax,betm(kl),ratha(kl),rathb(kl)
-      real sig,sigmh,dsig,rata,ratb,bet,tbar,tbardsig,dtmax,betm,
+      common/sigs/sig(kl),sigmh(kl),dsig(kl),rata(kl),ratb(kl),bet(kl)      &
+     & ,tbar(kl),tbardsig(kl),dtmax,betm(kl),ratha(kl),rathb(kl)
+      real sig,sigmh,dsig,rata,ratb,bet,tbar,tbardsig,dtmax,betm,           &
      &     ratha,rathb
      
