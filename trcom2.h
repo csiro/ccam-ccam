e8b0ea486f0e006913c77f483adb8df0f3885407
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-09-15 07:07:41 +0000

    Remove duplicate definition.

--- trcom2.h
+++ trcom2.h
@@ -1,5 +1,5 @@
       integer, parameter :: nstnmax=10, nstn2=3
-      integer mstn,nstn,istn,jstn,iunp,nstn2,istn2,jstn2,iunp2,
+      integer mstn,nstn,istn,jstn,iunp,istn2,jstn2,iunp2,
      &        iaustw,iauste,iaustn,iausts
       real slat,slon,slat2,slon2
       common / trcom2 / mstn,nstn,slat(nstnmax),slon(nstnmax),
