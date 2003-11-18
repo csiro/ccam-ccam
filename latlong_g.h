a9bce55ef03045143c28237c2a7227eca8afe5a4
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2003-11-18 04:23:15 +0000

    *** empty log message ***

---
+++ latlong_g.h
@@ -0,0 +1,4 @@
+      real rlatt_g(ifull_g),rlongg_g(ifull_g)
+      common/latlong_g/rlatt_g,rlongg_g
+!     rlatt and rlongg (radians) were in xyzinfo.h;
+!     eventually get rid of alat and along (degrees) from aalat.h
