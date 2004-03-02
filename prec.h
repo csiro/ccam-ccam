eb6aa6e5a1e1469c27e214a08ead9058caddabaa
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-02 00:47:46 +0000

    Use rnd_3hr instead of individual arrays (JMcG 2004)

--- prec.h
+++ prec.h
@@ -1,4 +1,2 @@
-      real precip,precc,rnd03,rnd06,rnd09,rnd12,rnd15,rnd18,rnd21
-      common/prec/precip(ifull),precc(ifull),
-     .            rnd03(ifull),rnd06(ifull),rnd09(ifull),
-     .            rnd12(ifull),rnd15(ifull),rnd18(ifull),rnd21(ifull)
+      real precip,precc,rnd_3hr
+      common/prec/precip(ifull),precc(ifull),rnd_3hr(ifull,8)
