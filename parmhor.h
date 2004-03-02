e927db75594b2926618c55307228380aafde735d
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-02 00:40:37 +0000

    Change mh_bs to 4 (JMcG 2004)

--- parmhor.h
+++ parmhor.h
@@ -1,18 +1,18 @@
 !     horizontal advection/staggering options (globpe, ints, staguv, upglobal)
       integer nt_adv, ndept
       common/parmhor/nt_adv,ndept
-      integer, parameter :: mhint=0  !  0 for simple; 2 for Bessel  (in ints)
+      integer, parameter :: mhint=0  !  0 for usual simple; 2 for Bessel  (in ints)
       
 !     for RMIP1 m_bs was -2; during 2002 it was 2
       integer, parameter :: m_bs=-2  !  0 for B&S off     usually -2
 !                               2 for B&S on (in ints)
 !                              -2 on for gases only
 !     m_bs is superseded on 23/7/03 by mh_bs
-      integer, parameter :: mh_bs=3  !  5 for B&S off     usually 4
-!                               4 for B&S on for gases (in ints)
-!                               3 for B&S on for T, gases 
-!                               2 for B&S on for u, v, T, gases 
-!                               1 for B&S on for psl, u, v, T, gases 
+      integer, parameter :: mh_bs=4  !  5 for B&S off     usually 4
+!                               4 for B&S on for qg, gases (in ints)
+!                               3 for B&S on for T, qg, gases 
+!                               2 for B&S on for u, v, T, qg, gases 
+!                               1 for B&S on for psl, u, v, T, qg, gases 
       integer, parameter :: mstagpt=-3 !  2 for 2-point
 !                               3 for 3-point
 !                              -3 for other (new) 3-point
