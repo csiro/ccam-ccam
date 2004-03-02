432769381ebb81598093c43454adc49622c2e6e8
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-02 00:32:35 +0000

    Add nh, npex (JMcG 2004)

--- parmdyn.h
+++ parmdyn.h
@@ -16,12 +16,13 @@
 !     this gives order for nritch advection options - not used nowadays
 
       integer m,mex,mfix,mfix_qg,mspec,mup,nonl,nritch,nritch_t,nrot,      &
-     &        nstag,nstagu,ntbar,nvsplit,nxmap,precon
+     &        nstag,nstagu,ntbar,nvsplit,nxmap,precon,nh,npex
       real    epsp,epsu,epsf,restol
       common/paramdyn/epsp,epsu,epsf,m,mex,mfix,mfix_qg,                   &
      &                mspec,mup,nonl,nritch,nritch_t,nrot,                 &
      &                nstag,nstagu,ntbar,                                  &
-     &                nvsplit,nxmap,restol,precon
+     &                nvsplit,nxmap,restol,precon,
+     &                nh,npex      ! for future use
 !              (ntbar=0)           ! 0 for standard
 !              (ntbar=(kl+1)/2)    ! level# for tbar2d with T set in nonlin
 !           nspec_us set to 1 by nritch=102 (unstaggering at opposite ktau)
