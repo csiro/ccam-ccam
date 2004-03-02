45d28fde8b3aec4885ff1ff13a8dc28d003ef39e
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-02 00:24:11 +0000

    Changed to use liquid water (JMcG 2004)

--- liqwpar.h
+++ liqwpar.h
@@ -1,5 +1,5 @@
-      integer, parameter :: ifullw=1
-c     if really doing liquid water variables use the following instead:
-c     integer, parameter :: ifullw=ifull+iextra
+!      integer, parameter :: ifullw=1  being removed from 29/1/04
+!     if really doing liquid water variables use the following instead:
+      integer, parameter :: ifullw=ifull+iextra
       real, dimension(ifullw,kl) :: qfg, qlg
       common/liqw/qfg,qlg
