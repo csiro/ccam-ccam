83f8d66007a1058b161b5a67bb1257cd5519c17f
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-11 01:29:23 +0000

    JMcG changes.

--- adjust5.f
+++ adjust5.f
@@ -1,10 +1,10 @@
       subroutine adjust5
       use cc_mpi
       use diag_m
-      use sumdd_m
       implicit none
       integer, parameter :: moistfix=2 ! 0 earlier; 1 for cube-root fix; 2 best, with ps
       integer, parameter :: mfix_rad=0 ! used to make gases 2 to ng add up to gas 1
+      integer, parameter :: ntest=0
       integer, parameter :: nys=0      ! for nys=1 use sun & yeh 1997 monotonic filter
 !                              within adjust5 on u,v
 !     nuv in parm.h:  now always 10
@@ -13,6 +13,8 @@
       include 'arrays.h'
       include 'const_phys.h'
       include 'indices.h'
+      include 'kuocom.h'   ! ldr
+      include 'liqwpar.h'  ! qfg,qlg
       include 'map.h'
       include 'morepbl.h'  ! condx,eg
       include 'nlin.h'
@@ -35,7 +37,7 @@
       common/epst/epst(ifull)
       real alph_p, alph_pm, delneg, delpos, alph_q
       common/mfixdiag/alph_p,alph_pm,delneg,delpos,alph_q
-      real :: delneg_l, delpos_l, sumdiffb_l  ! Local versions
+      real :: sumdiffb_l  ! Local versions
       real, dimension(3) :: delarr, delarr_l
       real tbar2d
       common/tbar2d/tbar2d(ifull)
@@ -43,8 +45,9 @@
       real p(ifull+iextra,kl),omgfnl(ifull,kl)
       real wrk1, wrk2  ! wrk2 not used here
       common/work3b/wrk1(ifull,kl),wrk2(ifull,kl)   ! just work arrays here
-      real qgsav, trsav
-      common/work3sav/qgsav(ifull,kl),trsav(ilt*jlt,klt,ngasmax) ! passed from nonlin
+      real qgsav, qfgsav, qlgsav, trsav
+      common/work3sav/qgsav(ifull,kl),qfgsav(ifull,kl),qlgsav(ifull,kl)
+     &             ,trsav(ilt*jlt,klt,ngasmax)  ! shared adjust5 & nonlin
       real zz(ifull),zzn(ifull),zze(ifull),zzw(ifull),
      &     zzs(ifull),pfact(ifull),alff(ifull+iextra),alf(ifull+iextra),
      &     alfe(ifull+iextra),alfn(ifull+iextra),alfu(ifull),alfv(ifull)
@@ -59,13 +62,11 @@
       equivalence (pextras,dpsldt)
 !     Save this so we can check whether initialisation needs to be redone
       real, save :: dtsave = 0.0
-      real hdt, hdtds, sum, qgminm, ratio, sumdiffb, alph_g
-      integer i, j, k, l, n, iq, ng, ierr
-#ifdef sumdd
-      complex, dimension(2) :: local_sum, global_sum
-!     Temporary array for the drpdr_local function
-      real, dimension(ifull) :: tmparr, tmparr2 
-#endif
+      real :: hdt, hdtds, sdmx, sdmx_g, sum, qgminm, ratio, sumdiffb,
+     &        alph_g
+      integer :: i, its, j, k, l, n, nits, nvadh_pass, iq, ng, ierr
+      real :: sumin, sumout, sumsav
+      real :: delpos_l, delneg_l
 
       call start_log(adjust_begin)
       hdt=dt/2.
@@ -85,7 +86,7 @@
          if ( mydiag ) then
             print *,'ux_stag ',ux(idjd,:)
             print *,'vx_stag ',vx(idjd,:)
-            write (6,"('qg ',19f7.3/(8x,19f7.3))") 
+            write (6,"('qg ',12f7.3/(8x,12f7.3))") 
      &             1000.*qg(idjd,:)
          end if
          call maxmin(alf,'a ',ktau,1.,1)
@@ -100,7 +101,7 @@
       endif
 
 !     recompute nonlinear sigma-dot contribution for updating tn, tx
-!     e contains intgrl{-dpsldt x dsigma} from 0 to sigma (dsig is -ve)
+!     e contains intgrl{-pslx x dsigma} from 0 to sigma (dsig is -ve)
 !     vert. integ. nonlin part of mass weighted div into e
       do k=1,kl
        do iq=1,ifull
@@ -112,7 +113,7 @@
       enddo     ! iq loop
       do k=kl-1,1,-1
        do iq=1,ifull
-        e(iq,k)=e(iq,k+1)+dsig(k)*pslx(iq,k)  ! i.e. M_bar_sig-.5
+        e(iq,k)=e(iq,k+1)+dsig(k)*pslx(iq,k)  ! i.e. -M_bar_(sig-.5)
        enddo    ! iq loop
       enddo     ! k loop
       do iq=1,ifull
@@ -289,31 +290,31 @@
      &              u(1:ifull,:),v(1:ifull,:)) ! usual
 
       if( (diag.or.nmaxpr.eq.1) .and. mydiag ) then
-         write (6,"('div_adj(1-9) ',9f8.2)") (d(idjd,k)*1.e6,k=1,9)
-         write (6,"('omgfnl*dt    ',9f8.4)") (omgfnl(idjd,k)*dt,k=1,9)
+         write (6,"('diva ',9f8.2/4x,9f8.2)") d(idjd,:)*1.e6
+         write (6,"('omgfnl*dt    ',9f8.4)") omgfnl(idjd,:)*dt
       endif
 
 !     vert. integ. div into e
       do iq=1,ifull
-       e(iq,kl)=-dsig(kl)*d(iq,kl)
+       wrk2(iq,kl)=-dsig(kl)*d(iq,kl)  ! nh
       enddo     ! iq loop
       do k=kl-1,1,-1
        do iq=1,ifull
-        e(iq,k)=e(iq,k+1)-dsig(k)*d(iq,k)
+        wrk2(iq,k)=wrk2(iq,k+1)-dsig(k)*d(iq,k)
        enddo    ! iq loop
       enddo     ! k  loop
 
 !     full-level omega/ps into omgf (linear part only)
       do k=1,kl-1
        do iq=1,ifull
-        omgf(iq,k)=-rata(k)*e(iq,k+1)-ratb(k)*e(iq,k)
+        omgf(iq,k)=-rata(k)*wrk2(iq,k+1)-ratb(k)*wrk2(iq,k)
        enddo    ! iq loop
       enddo     ! k  loop
 
       do iq=1,ifull
-       omgf(iq,kl)=-ratb(kl)*e(iq,kl)
+       omgf(iq,kl)=-ratb(kl)*wrk2(iq,kl)
        pslsav(iq)=psl(iq)   ! in work2 (not last because of vadvtvd)
-       psl(iq)=pslxint(iq)-hdt*e(iq,1)  *(1.+epst(iq))
+       psl(iq)=pslxint(iq)-hdt*wrk2(iq,1)  *(1.+epst(iq))
       enddo     ! iq loop
 
       do k=1,kl
@@ -345,24 +346,37 @@
      .           +hdt*(1.+epst(iq))*tbar2d(iq)*omgf(iq,k)*roncp/sig(k)
        enddo    ! iq loop
       enddo     ! k  loop
+      if((diag.or.nmaxpr.eq.1).and.mydiag)then
+        write (6,"('sdota',9f8.3/5x,9f8.3)") sdot(idjd,1:kl)
+        write (6,"('omgfa',9f8.3/5x,9f8.3)")
+     .            ps(idjd)*dpsldt(idjd,1:kl)
+      endif
 
-      if(nvadh.eq.2)then                 ! final dt/2 's worth
+      if(nvadh.eq.2.and.nvad.gt.0)then                 ! final dt/2 's worth
         if ( (diag.or.nmaxpr.eq.1) .and. mydiag ) then
          print *,'before vertical advection in adjust5'
-         write (6,"('sdot',9f8.3/4x,9f8.3)") sdot(idjd,:)
          write (6,"('t   ',9f8.2/4x,9f8.2)") t(idjd,:)
          write (6,"('u   ',9f8.2/4x,9f8.2)") u(idjd,:)
          write (6,"('v   ',9f8.2/4x,9f8.2)") v(idjd,:)
          write (6,"('qg  ',9f8.3/4x,9f8.3)") 1000.*qg(idjd,:)
         endif
-        ! For now use this form of call so that vadvtvd doesn't need to 
-        ! be changed
-        ! Note that use of same arrays for input and output is strictly a
-        ! violation of fortran standard
-        if(nvad.eq.4) call vadvtvd(t(1:ifull,:),
-     &                             u(1:ifull,:),
-     &                             v(1:ifull,:))
-        if(nvad.eq.7) call vadv30(t(1:ifull,:),
+        if(nvad.eq.4)then
+          sdmx = maxval(abs(sdot))
+          call MPI_AllReduce(sdmx, sdmx_g, 1, MPI_REAL, MPI_MAX, 0,
+     &                       MPI_COMM_WORLD, ierr )
+          nits=1+sdmx_g/nvadh
+          nvadh_pass=nvadh*nits
+          if(mydiag.and.mod(ktau,nmaxpr).eq.0)
+     &      print *,'in upglobal sdmx,nits,nvadh_pass ',
+     &                           sdmx_g,nits,nvadh_pass
+          do its=1,nits
+           ! For now use this form of call so that vadvtvd doesn't need to 
+           ! be changed. With assumed shape arguments this wouldn't be necessary
+            call vadvtvd(t(1:ifull,:),u(1:ifull,:),v(1:ifull,:),
+     &                   nvadh_pass)
+          enddo
+        endif  ! (nvad.eq.4)
+        if(nvad.ge.7) call vadv30(t(1:ifull,:),
      &                            u(1:ifull,:),
      &                            v(1:ifull,:)) ! for vadvbess
         if( ( diag.or.nmaxpr.eq.1) .and. mydiag ) then
@@ -374,7 +388,7 @@
         write (6,"('u   ',9f8.2/4x,9f8.2)") u(idjd,:)
         write (6,"('v   ',9f8.2/4x,9f8.2)") v(idjd,:)
         endif
-      endif     !  (nvadh.eq.2)then
+      endif     !  (nvadh.eq.2.and.nvad.gt.0)
 
 !     could call nestin here (using xarrs instead of davb.h)
       if(mspec.eq.1.and.nbd.ne.0)call davies  ! before mfix mass fix in C-C
@@ -383,34 +397,14 @@
 !         delneg is the sum of all negative changes over globe
 !         alph_p is chosen to satisfy alph_p*delpos + delneg/alph_p = 0
 !         _l means local to this processor        
-         delpos_l = 0.
-         delneg_l = 0.
          do iq=1,ifull
             delps(iq) = psl(iq)-pslsav(iq)
-#ifdef sumdd         
-            tmparr(iq)  = max(0.,delps(iq)*wts(iq))
-            tmparr2(iq) = min(0.,delps(iq)*wts(iq))
-#else
-            delpos_l = delpos_l + max(0.,delps(iq)*wts(iq))
-            delneg_l = delneg_l + min(0.,delps(iq)*wts(iq))
-#endif
-         enddo
-#ifdef sumdd
-         local_sum = (0.,0.)
-         call drpdr_local(tmparr, local_sum(1))
-         call drpdr_local(tmparr2, local_sum(2))
-         call MPI_Allreduce ( local_sum, global_sum, 2, MPI_COMPLEX,
-     &                        MPI_SUMDR, MPI_COMM_WORLD, ierr )
-         delpos = real(global_sum(1))
-         delneg = real(global_sum(2))
-#else
-
-         delarr_l(1:2) = (/ delpos_l, delneg_l /)
-         call MPI_Allreduce ( delarr_l, delarr, 2, MPI_REAL, MPI_SUM,
-     &                        MPI_COMM_WORLD, ierr )
-         delpos = delarr(1)
-         delneg = delarr(2)
-#endif
+         end do
+         call ccglobal_posneg(delps,delpos,delneg)
+	 if(ntest.eq.1)then
+            call ccglobal_sum(pslsav,sumsav)
+            call ccglobal_sum(psl,sumin)
+	 endif  ! (ntest.eq.1)
          if(mfix.eq.1)then
             alph_p = sqrt( -delneg/delpos)
             alph_pm=1./alph_p
@@ -428,6 +422,13 @@
             psl(iq) = pslsav(iq) +
      &           alph_p*max(0.,delps(iq)) + alph_pm*min(0.,delps(iq))
          enddo
+	 if(ntest.eq.1)then
+           if(myid==0) print *,'psl_delpos,delneg,alph_p,alph_pm ',
+     &                 delpos,delneg,alph_p,alph_pm
+           call ccglobal_sum(psl,sumout)
+           if(myid==0)
+     &        print *,'psl_sumsav,sumin,sumout ',sumsav,sumin,sumout
+	 endif  ! (ntest.eq.1)
       endif                     !  mfix.gt.0
 
       do iq=1,ifull
@@ -452,76 +453,98 @@
            enddo   ! iq loop
           enddo    ! k  loop
         endif      ! (moistfix.eq.1)
-!  	 default now with ps weighting
+!  	 default is moistfix=2 now, with ps weighting
         do k=1,kl
 	  do iq=1,ifull
-          qg(iq,k)=qg(iq,k)*ps(iq)
-          qgsav(iq,k)=qgsav(iq,k)*aa(iq)
+            qg(iq,k)=qg(iq,k)*ps(iq)
+            qgsav(iq,k)=qgsav(iq,k)*aa(iq)
          enddo   ! iq loop
         enddo    ! k  loop
+        if(ntest.eq.1)then
+          call ccglobal_sum(qgsav,sumsav)
+          call ccglobal_sum(qg,sumin)
+	endif  ! (ntest.eq.1)
 !       perform conservation fix on qg, as affected by vadv, hadv, hordif
 !       N.B. won't cope with any -ves from conjob
 !       delpos is the sum of all positive changes over globe
 !       delneg is the sum of all negative changes over globe
-        delpos_l=0.
-        delneg_l=0.
-c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
-#ifdef sumdd
-        local_sum = (0.,0.)
-#endif
         do k=1,kl
            do iq=1,ifull
               wrk1(iq,k)=max(qg(iq,k),qgminm*ps(iq))-qgsav(iq,k) ! increments
-#ifdef sumdd
-              tmparr(iq)  = max(0.,-dsig(k)*wrk1(iq,k)*wts(iq))
-              tmparr2(iq) = min(0.,-dsig(k)*wrk1(iq,k)*wts(iq))
-#else
-              delpos_l = delpos_l + max(0.,-dsig(k)*wrk1(iq,k)*wts(iq))
-              delneg_l = delneg_l + min(0.,-dsig(k)*wrk1(iq,k)*wts(iq))
-#endif
            enddo                ! iq loop
-#ifdef sumdd
-           call drpdr_local(tmparr, local_sum(1))
-           call drpdr_local(tmparr2, local_sum(2))
-#endif
         enddo                   ! k loop
-#ifdef sumdd
-         call MPI_Allreduce ( local_sum, global_sum, 2, MPI_COMPLEX,
-     &                        MPI_SUMDR, MPI_COMM_WORLD, ierr )
-         delpos = real(global_sum(1))
-         delneg = real(global_sum(2))
-#else
-        delarr_l(1:2) = (/ delpos_l, delneg_l /)
-        call MPI_Allreduce ( delarr_l, delarr, 2, MPI_REAL, MPI_SUM,
-     &                       MPI_COMM_WORLD, ierr )
-        delpos = delarr(1)
-        delneg = delarr(2)
-#endif
+        call ccglobal_posneg(wrk1,delpos,delneg)
         ratio = -delneg/delpos
-        if(mfix_qg.eq.1)alph_q = min(ratio,sqrt(ratio))  ! why min?
+        if(mfix_qg.eq.1)alph_q = min(ratio,sqrt(ratio))  ! best option
         if(mfix_qg.eq.2)alph_q = sqrt(ratio)
         do k=1,kl        ! this is cunning 2-sided scheme
          do iq=1,ifull
-         qg(iq,k)=qgsav(iq,k)+
-     .     alph_q*max(0.,wrk1(iq,k)) + min(0.,wrk1(iq,k))/max(1.,alph_q)
+           qg(iq,k)=qgsav(iq,k)+
+     &       alph_q*max(0.,wrk1(iq,k))+min(0.,wrk1(iq,k))/max(1.,alph_q)
          enddo   ! iq loop
         enddo    ! k  loop
         if(moistfix.eq.1)then
-         do k=1,kl
-          do iq=1,ifull
-           qg(iq,k)=qg(iq,k)**3
-          enddo   ! iq loop
-         enddo    ! k  loop
+          qg(:,:)=qg(:,:)**3
         endif      ! (moistfix.eq.1)
+        if(ntest.eq.1)then
+	  if(myid==0) print *,'qg_delpos,delneg,ratio,alph_q ',
+     &                         delpos,delneg,ratio,alph_q
+          call ccglobal_sum(qg,sumout)
+	  if(myid==0) print *,'qg_sumsav,sumin,sumout ',sumsav,sumin,sumout
+	 endif  ! (ntest.eq.1)
+
 !  	 undo ps weighting
         do k=1,kl
 	  do iq=1,ifull
-          qg(iq,k)=qg(iq,k)/ps(iq)
-         enddo   ! iq loop
+            qg(iq,k)=qg(iq,k)/ps(iq)
+          enddo   ! iq loop
         enddo    ! k  loop
 	 delpos=delpos/1.e5
  	 delneg=delneg/1.e5  ! for diag print in globpe
-      endif        !  (mfix_qg.gt.0)
+      endif        !  (mfix_qg.gt.0.and.mspec.eq.1)
+
+      if(mfix_qg.gt.0.and.mspec.eq.1.and.ldr.ne.0)then
+        do k=1,kl
+          qfg(1:ifull,k)=qfg(1:ifull,k)*ps(1:ifull)
+          qfgsav(1:ifull,k)=qfgsav(1:ifull,k)*aa(1:ifull)
+          qlg(1:ifull,k)=qlg(1:ifull,k)*ps(1:ifull)
+          qlgsav(1:ifull,k)=qlgsav(1:ifull,k)*aa(1:ifull)
+        enddo    ! k  loop
+!       perform conservation fix on qfg, qlg as affected by vadv, hadv, hordif
+!       N.B. won't cope with any -ves from conjob
+!       delpos is the sum of all positive changes over globe
+!       delneg is the sum of all negative changes over globe
+        do k=1,kl
+           do iq=1,ifull
+              wrk1(iq,k)=max(qfg(iq,k),0.)-qfgsav(iq,k) ! increments
+         enddo   ! iq loop
+        enddo    ! k loop
+        call ccglobal_posneg(wrk1,delpos,delneg)
+        ratio = -delneg/max(delpos,1.e-40)
+        if(mfix_qg.eq.1)alph_q = min(ratio,sqrt(ratio))  ! best option
+        if(mfix_qg.eq.2)alph_q = sqrt(ratio)
+!       this is cunning 2-sided scheme
+        qfg(1:ifull,:)=qfgsav(:,:)+
+     .     alph_q*max(0.,wrk1(:,:)) + min(0.,wrk1(:,:))/max(1.,alph_q)
+        do k=1,kl
+         do iq=1,ifull
+          wrk1(iq,k)=max(qlg(iq,k),0.)-qlgsav(iq,k)  ! increments
+         enddo   ! iq loop
+        enddo    ! k loop
+        call ccglobal_posneg(wrk1,delpos,delneg)
+        ratio = -delneg/max(delpos,1.e-40)
+        if(mfix_qg.eq.1)alph_q = min(ratio,sqrt(ratio))  ! best option
+        if(mfix_qg.eq.2)alph_q = sqrt(ratio)
+!       this is cunning 2-sided scheme
+        qlg(1:ifull,:)=qlgsav(:,:)+
+     .     alph_q*max(0.,wrk1(:,:)) + min(0.,wrk1(:,:))/max(1.,alph_q)
+!  	 undo ps weighting
+        do k=1,kl
+         qfg(1:ifull,k)=qfg(1:ifull,k)/ps(1:ifull)
+         qlg(1:ifull,k)=qlg(1:ifull,k)/ps(1:ifull)
+        enddo    ! k  loop
+      endif      !  (mfix_qg.gt.0.and.mspec.eq.1.and.ldr.ne.0)
+
 
       if(mfix_qg.gt.0.and.mspec.eq.1.and.ngas.ge.1)then
 !       perform conservation fix on tr1,tr2 as affected by vadv, hadv, hordif
@@ -533,21 +556,13 @@ c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
            trsav(iq,k,ng)=trsav(iq,k,ng)*aa(iq)
           enddo   ! iq loop
          enddo    ! k  loop
-         delpos_l = 0.
-         delneg_l = 0.
          do iq=1,ifull
           do k=1,kl
            wrk1(iq,k)=max(tr(iq,k,ng),gasmin(ng)*ps(iq))
      .                                         -trsav(iq,k,ng)  ! has increments
-           delpos_l = delpos_l + max(0.,-dsig(k)*wrk1(iq,k)/em(iq)**2)
-           delneg_l = delneg_l + min(0.,-dsig(k)*wrk1(iq,k)/em(iq)**2)
           enddo   ! k loop
          enddo    ! iq loop
-         delarr_l(1:2) = (/ delpos_l, delneg_l /)
-         call MPI_Allreduce ( delarr_l, delarr, 2, MPI_REAL, MPI_SUM,
-     &                        MPI_COMM_WORLD, ierr )
-         delpos = delarr(1)
-         delneg = delarr(2)
+         call ccglobal_posneg(wrk1,delpos,delneg)
          ratio = -delneg/delpos
          if(mfix_qg.eq.1)alph_g = min(ratio,sqrt(ratio))  ! why min?
          if(mfix_qg.eq.2)alph_g = sqrt(ratio)
@@ -557,14 +572,11 @@ c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
      .     alph_g*max(0.,wrk1(iq,k)) + min(0.,wrk1(iq,k))/max(1.,alph_g)
           enddo   ! iq loop
          enddo    ! k  loop
-         if(moistfix.eq.2)then  !  with ps weighting
-           do k=1,kl
-	     do iq=1,ifull
+         do k=1,kl
+           do iq=1,ifull
              tr(iq,k,ng)=tr(iq,k,ng)/ps(iq)
-             trsav(iq,k,ng)=trsav(iq,k,ng)/ps(iq)
-            enddo   ! iq loop
-           enddo    ! k  loop
-         endif      ! (moistfix.eq.2)
+           enddo   ! iq loop
+         enddo    ! k  loop
         enddo    ! ng loop
         if(mfix_rad.gt.0)then  ! to make gases 2 to ng add up to gas 1
          do k=1,kl
@@ -572,7 +584,9 @@ c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
            sumdiffb_l = 0.
            delpos_l = 0.
            delneg_l = 0.
+           trsav(iq,k,1)=trsav(iq,k,1)/ps(iq)
            do ng=2,ngas        
+            trsav(iq,k,ng)=trsav(iq,k,ng)/ps(iq)
             sumdiffb_l = sumdiffb_l + tr(iq,k,ng)
             delpos_l = delpos_l+max( 1.e-20,tr(iq,k,ng)-trsav(iq,k,ng))
             delneg_l = delneg_l+min(-1.e-20,tr(iq,k,ng)-trsav(iq,k,ng))
@@ -601,7 +615,7 @@ c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
 !            Would require bounds call
 !            print  *,'adjust5 d: e ',d(ie(iq),:)
 !            print  *,'adjust5 d: w ',d(iw(iq),:)
-            print  *,'adjust5 idjd,e ',idjd, e(idjd,:)
+            print  *,'adjust5 idjd,e ',idjd, wrk2(idjd,:)
             print  *,'adjust5 psl & n e w s',psl(iq),
      &          psl(in(iq)),psl(ie(iq)),psl(iw(iq)),psl(is(iq))
          end if
@@ -619,7 +633,7 @@ c       print *,'qgsav,qg_in',qgsav(idjd,1),qg(idjd,1,1)
          call printa('dps ',aa,ktau,0,ia,ib,ja,jb,0.,.01)
          call printa('ps  ',ps,ktau,0,ia,ib,ja,jb,1.e5,.01)
          if ( mydiag ) then
-            write (6,"('qg ',19f7.3/(8x,19f7.3))") 1000.*qg(idjd,:)
+            write (6,"('qg ',12f7.3/(8x,12f7.3))") 1000.*qg(idjd,:)
          end if
          if(sig(nlv).lt..3)then
             call printa('qg  ',qg,ktau,nlv,ia,ib,ja,jb,0.,1.e6)
