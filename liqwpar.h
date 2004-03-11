c694f9a5f0985e22eecba20ef3e270d6d254fe66
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-03-11 01:33:34 +0000

    Make use of iextra consistent with rest of model.

--- liqwpar.h
+++ liqwpar.h
@@ -1,5 +1,5 @@
 !      integer, parameter :: ifullw=1  being removed from 29/1/04
 !     if really doing liquid water variables use the following instead:
-      integer, parameter :: ifullw=ifull+iextra
-      real, dimension(ifullw,kl) :: qfg, qlg
+      integer, parameter :: ifullw=ifull
+      real, dimension(ifull+iextra,kl) :: qfg, qlg
       common/liqw/qfg,qlg
