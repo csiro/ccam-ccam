00b054154e44a1234b06206ae15a522af3bc781c
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-05-06 05:08:33 +0000

    JMcG change - May 2004

--- histave.h
+++ histave.h
@@ -1,5 +1,8 @@
       real, dimension(ifull) :: eg_ave, fg_ave, ga_ave, epot_ave,
-     &        cbas_ave, ctop_ave, qscrn_ave, tmaxscr, tminscr, tscr_ave
+     &                          cbas_ave, ctop_ave, rndmax, qscrn_ave,
+     &                          tmaxscr, tminscr, tscr_ave,
+     &                          riwp_ave, rlwp_ave
 
-      common/histave/eg_ave,fg_ave,ga_ave,epot_ave,cbas_ave,ctop_ave,
-     &    qscrn_ave,tmaxscr,tminscr,tscr_ave
+      common /histave/ eg_ave, fg_ave, ga_ave, epot_ave, cbas_ave,
+     &                 ctop_ave, rndmax, qscrn_ave, tmaxscr, tminscr,
+     &                 tscr_ave, riwp_ave, rlwp_ave
