94418b97fea72c02edeb213f0e8569ce9374b063
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2004-05-06 05:07:43 +0000

    *** empty log message ***

---
+++ histave.h
@@ -0,0 +1,5 @@
+      real, dimension(ifull) :: eg_ave, fg_ave, ga_ave, epot_ave,
+     &        cbas_ave, ctop_ave, qscrn_ave, tmaxscr, tminscr, tscr_ave
+
+      common/histave/eg_ave,fg_ave,ga_ave,epot_ave,cbas_ave,ctop_ave,
+     &    qscrn_ave,tmaxscr,tminscr,tscr_ave
