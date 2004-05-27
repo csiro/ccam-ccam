7a231dbb7e70159134e80713b59f6658f62d7011
McGregor, John (O&A, Aspendale) <John.Mcgregor@csiro.au>
2004-05-27 07:10:18 +0000

    Include file for raddiag common block

---
+++ raddiag.h
@@ -0,0 +1,9 @@
+      real, dimension(ifull) :: sint_ave, sot_ave, soc_ave, sgn_ave,
+     &                           rtu_ave, rtc_ave, rgn_ave, rgc_ave,
+     &                           cld_ave, cll_ave, clm_ave, clh_ave
+      integer koundiag
+      common/raddiag/sint_ave, sot_ave , soc_ave ,
+     &               sgn_ave , rtu_ave , rtc_ave ,
+     &               rgn_ave , rgc_ave , cld_ave , 
+     &               cll_ave , clm_ave , clh_ave , 
+     &               koundiag
