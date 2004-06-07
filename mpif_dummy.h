0e199b53e9bbee8b408402a5c3a8bbdd75881e95
McGregor, John (O&A, Aspendale) <John.Mcgregor@csiro.au>
2004-06-07 04:36:33 +0000

    Files for building without a real MPI library.

---
+++ mpif_dummy.h
@@ -0,0 +1,30 @@
+! This is a cut down version of the MPICH mpif.h, suitable for CCAM using
+! mpidummy.f90 in place of a real MPI library.
+
+!
+!     The types MPI_INTEGER1,2,4 and MPI_REAL4,8 are OPTIONAL.
+!     Their values are zero if they are not available.  Note that
+!     using these reduces the portability of code.
+!
+      integer, parameter :: MPI_LOGICAL   = 25
+      integer, parameter :: MPI_REAL      = 26
+      integer, parameter :: MPI_DOUBLE_PRECISION = 27
+      integer, parameter :: MPI_INTEGER   = 28
+!
+!     Collective operations
+!
+      integer, parameter :: MPI_MAX    = 100
+      integer, parameter :: MPI_MIN    = 101
+      integer, parameter :: MPI_SUM    = 102
+!
+!     Communicator
+!
+      integer, parameter :: MPI_COMM_WORLD  = 91
+      
+      integer, parameter :: MPI_STATUS_SIZE = 6
+
+!
+!     All other MPI routines are subroutines
+!
+      double precision mpi_wtime, mpi_wtick
+      external         MPI_WTIME, MPI_WTICK
