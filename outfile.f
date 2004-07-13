5c9d1a7905c5954b7b214b8cb226f0d36bfa6cc0
McGregor, John (O&A, Aspendale) <John.Mcgregor@csiro.au>
2004-07-13 05:10:17 +0000

    Restore missing surface fields.

--- outfile.f
+++ outfile.f
@@ -106,6 +106,81 @@ c     reincorporate land mask into surface temperature
          print*, "Error, binary output not supported"
          stop
       end if
+
+      if(nrungcm.eq.-2.or.nrungcm.eq.-3.or.nrungcm.eq.-5)then
+         if(ktau.eq.nwrite/2.or.ktau.eq.nwrite)then
+!        usually after first 24 hours, save soil variables for next run
+            if(ktau.eq.nwrite)then  ! 24 hour write
+               if(ktime.eq.1200)then
+                  co2out=co2_12     ! 'co2.1200'
+                  radonout=radon_12 ! 'radon.1200'
+                  surfout=surf_12   ! 'current.1200'
+                  qgout='qg_12'
+               else
+                  co2out=co2_00     !  'co2.0000'
+                  radonout=radon_00 ! 'radon.0000'
+                  surfout=surf_00   ! 'current.0000'
+                  qgout='qg_00'
+               endif
+            else                    ! 12 hour write
+               if(ktime.eq.1200)then
+                  co2out=co2_00     !  'co2.0000'
+                  radonout=radon_00 ! 'radon.0000'
+                  surfout=surf_00   ! 'current.0000'
+                  qgout='qg_00'
+               else
+                  co2out=co2_12     ! 'co2.1200'
+                  radonout=radon_12 ! 'radon.1200'
+                  surfout=surf_12   ! 'current.1200'
+                  qgout='qg_12'
+               endif
+            endif               ! (ktau.eq.nwrite)
+            if ( myid == 0 ) then
+               print *,'writing current soil & snow variables to ',
+     &                  surfout
+               open(unit=77,file=surfout,form='formatted',
+     &              status='unknown')
+               write (77,*) kdate,ktime,' ktau = ',ktau
+            end if
+            call writeglobvar(77, wb, fmt='(14f6.3)')
+            call writeglobvar(77, tgg, fmt='(12f7.2)')
+            call writeglobvar(77, tss, fmt='(12f7.2)')
+            call writeglobvar(77, snowd, fmt='(12f7.1)')
+            call writeglobvar(77, sicedep, fmt='(12f7.1)')
+            if ( myid == 0 ) close (77)
+            if(ico2.gt.0)then
+               ico2x=max(1,ico2)
+               if ( myid == 0 ) then
+                  open(unit=77,file=co2out,form='formatted',
+     &                 status='unknown')
+                  write (77,*) kdate,ktime,' ktau = ',ktau
+               end if
+               call writeglobvar(77, tr(:,:,ico2x), fmt='(12f7.2)')
+               if ( myid==0 ) close (77)
+            endif               ! (ico2.gt.0)
+            if(iradon.gt.0)then
+               iradonx=max(1,iradon)
+               if ( myid == 0 ) then
+                  open(unit=77,file=radonout,form='formatted',
+     &                 status='unknown')
+                  write (77,*) kdate,ktime,' ktau = ',ktau
+               end if
+               call writeglobvar (77, tr(:,:,iradonx), fmt='(12f7.1)')
+               if ( myid == 0 ) close (77)
+            endif               ! (ico2.gt.0)
+            if(nrungcm.eq.-2.or.nrungcm.eq.-5)then
+               if ( myid == 0 ) then
+                  print *,'writing special qgout file: ',qgout
+                  open(unit=77,file=qgout,form='unformatted',
+     &                 status='unknown')
+               end if
+               call writeglobvar(77, qg)
+               if ( myid == 0 ) close (77)
+         endif  ! (nrungcm.eq.-2.or.nrungcm.eq.-5)
+        endif  ! (ktau.eq.nwrite/2.or.ktau.eq.nwrite)
+      endif    ! (nrungcm.eq.-2.or.nrungcm.eq.-3.or.nrungcm.eq.-5)
+
+
 c---------------------------------------------------------------------------
       if(io_outt.eq.1)then  ! for netcdf
          ms_out = 0             ! Not set anywhere else ?????
