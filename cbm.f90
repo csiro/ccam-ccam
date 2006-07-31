6dc5f1706eb7887af5e547f79b6b96856bc7ca1e
Dix, Martin (O&A, Aspendale) <Martin.Dix@csiro.au>
2006-07-31 22:44:54 +0000

    Remove unused debugging diagnostics

--- cbm.f90
+++ cbm.f90
@@ -1271,18 +1271,6 @@ CONTAINS
     TYPE (veg_parameter_type), INTENT(INOUT)	:: veg	
     INTEGER(i_d) :: ijd
 
-
-!!!! temp
-REAL(r_1), dimension(mp)              :: xx1,xx2,xx3
-  INTEGER(i_d)                          :: u         ! output unit
-  LOGICAL                               :: is_open   ! Is file open?
-  REAL(r_1), DIMENSION(mp)              :: owbtot
-  REAL(r_1), DIMENSION(mp)              :: swnet
-  REAL(r_1), DIMENSION(mp)              :: lwnet
-  INTEGER(i_d),DIMENSION(10) :: listp !
-
-  include 'morepbl.h'
-
     veg%meth = 0
     ijd =3688
 
@@ -1323,87 +1311,6 @@ REAL(r_1), dimension(mp)              :: xx1,xx2,xx3
     ! Set net ecosystem exchange after adjustments to frs:
     canopy%fnee = canopy%fpn + canopy%frs + canopy%frp
   
-!!!!temp
-
-!  listp(1)=2726
-!  listp(2)=3002
-!  listp(3)=3009
-!  listp(4)=2468
-!  listp(5)=2560
-!  listp(6)=1547
-!  listp(7)=3688
-!
-! u = 98
-!  INQUIRE (u, opened=is_open)
-!  IF (.not. is_open) THEN
-!     OPEN (u, file='f98.txt', status='replace')
-!     WRITE(u, '(a6,60a14)') &
-!          'ktau', 'hod', 'fsd', 'fld', 'precip', & !1-5
-!          'tk', 'pmb', 'ua', 'qv', 'ca', & !6-11
-!          'swnet', 'lwnet', &
-!          'fe', 'fh', & !12-14
-!          'fnv+fns', 'trad', 'tv', & !15-17
-!          'acond', 'wb(1,1)', 'wb(1,2)', & !18-20
-!          'wb(1,3)', 'wb(1,4)', 'wb(1,5)', 'wb(1,6)', & !21-24
-!          'xx1', 'xx2', 'xx3', & !25-27
-!          'tgg(1,1)', 'tgg(1,2)', 'tgg(1,3)', 'tgg(1,4)', & !28-31
-!          'tgg(1,5)', 'tgg(1,6)', & !32-33
-!          'fns', 'fes', 'fhs', 'ga', & !34-37
-!          'frp', 'frpw', 'frpr', 'fpn', 'frs', & !38-42
-!          'pfrs', &
-!          'fnv', 'fev', 'fevc', 'fevw', 'fhv', & !43-47
-!          'precis', 'vlai', 'ebal', 'ebalcan', 'ebalcan2', 'ebalsoil', & !48-53
-!          'runoff', 'rnof1', 'rnof2', 'delwc', 'snowd', & !54-58
-!          'wbal', 'albedo(:,1)', 'albedo(:,2)' !59-61
-!  END IF
-!  xx1=ssoil%tggsn(:,1)
-!  xx2=ssoil%tggsn(:,2)
-!  xx3=ssoil%tggsn(:,3)
-!  xx2=ssoil%tggsn(:,2)
-!  where (ssoil%isflag == 0 )
-!     xx1=ssoil%tgg(:,1)
-!     xx2=ssoil%tgg(:,2)
-!     xx3=ssoil%tgg(:,3)
-!  end where
-
-! swnet=SUM(rad%qcan(:,:,1),2)+SUM(rad%qcan(:,:,2),2)+rad%qssabs
-!  lwnet=met%fld-sboltz*emleaf*canopy%tv**4 *(1-rad%transd)-rad%flws*rad%transd
-!  do k=1,mp
-!    swnet(k)=SUM(rad%qcan(k,:,1))+SUM(rad%qcan(k,:,2))+rad%qssabs(k)
-!    lwnet(k)=met%fld(k)-sboltz*emleaf*canopy%tv(k)**4 *(1-rad%transd(k))-mout%flws(k)*rad%transd(k)
-!  enddo
-!  do k=1,7
-!   ijd =listp(k)
-!
-!
-!     WRITE(u, '(i5,"xr",i7,65e14.5)') &
-!          ijd,ktau,met%hod(ijd),met%fsd(ijd),met%fld(ijd),met%precip(ijd), & ! 1-5
-!          met%tk(ijd), met%pmb(ijd),met%ua(ijd),met%qv(ijd),met%ca(ijd), & ! 6-11
-!          swnet(ijd),lwnet(ijd), &
-!          canopy%fe(ijd),canopy%fh(ijd),&                                 !12-14
-!          canopy%fnv(ijd)+canopy%fns(ijd), rad%trad(ijd),canopy%tv(ijd), & !15-17
-!!          1./max(0.0001,rough%rt1(ijd)), ssoil%wb(ijd,1),ssoil%wb(ijd,2),& !18-20
-!          ssoil%wb(ijd,3),ssoil%wb(ijd,4), ssoil%wb(ijd,5),ssoil%wb(ijd,6),& !21-24
-!          xx1(ijd),xx2(ijd),xx3(ijd),      &   !25-27
-!          ! 0.0, 0.0, 0.0,        &                       !25-27
-!          ssoil%tgg(ijd,1),ssoil%tgg(ijd,2), ssoil%tgg(ijd,3),ssoil%tgg(ijd,4),& !28-31
-!          ssoil%tgg(ijd,5),ssoil%tgg(ijd,6),    & !32-33
-!          canopy%fns(ijd),canopy%fes(ijd),canopy%fhs(ijd),canopy%ga(ijd), & !34-37
-!          canopy%frp(ijd),canopy%frpw(ijd),canopy%frpr(ijd),canopy%fpn(ijd),canopy%frs(ijd),& !38-42
-!          canopy%fnv(ijd),canopy%fev(ijd),canopy%fevc(ijd),canopy%fevw(ijd),canopy%fhv(ijd), & !43-47
-!          canopy%precis(ijd),veg%vlai(ijd),bal%ebal(ijd), &
-!          canopy%fnv(ijd)-canopy%fhv(ijd)-canopy%fev(ijd), &
-!          bal%drybal(ijd)+bal%wetbal(ijd), &
-!          canopy%fns(ijd)-canopy%fhs(ijd)-canopy%fes(ijd)*ssoil%cls(ijd)-canopy%ga(ijd), &
-!          ssoil%runoff(ijd),ssoil%rnof1(ijd),ssoil%rnof2(ijd),canopy%delwc(ijd),ssoil%snowd(ijd), &
-!          bal%wbal(ijd),rad%albedo(ijd,1),rad%albedo(ijd,2) , &
-!         1./max(0.001,ssoil%rtsoil(ijd)), canopy%us(ijd),canopy%cduv(ijd), pblh(ijd)
-!        enddo
-
-
-
-!!!! temp
-
 END SUBROUTINE cbm
 
 END MODULE cbm_module
