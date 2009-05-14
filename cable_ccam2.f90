1aaadde4e19f2c7a2475cd244235bfc28b2e63d2
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2009-05-14 02:56:51 +0000

    Removed comment from spitter call

--- cable_ccam2.f90
+++ cable_ccam2.f90
@@ -126,7 +126,7 @@ module cable_ccam
        met%fsd(:,3)=sgsave(cmap)/(1.-0.5*sum(albvisnir(cmap,:),2))! short wave down (positive) W/m^2       
        met%fsd(:,1)=swrsave*met%fsd(:,3)
        met%fsd(:,2)=(1.-swrsave)*met%fsd(:,3)
-       rad%fbeam(:,3)=spitterx(met%doy,met%coszen,met%fsd(:,3)) ! check
+       rad%fbeam(:,3)=spitterx(met%doy,met%coszen,met%fsd(:,3)) 
        rad%fbeam(:,1)=rad%fbeam(:,3)
        rad%fbeam(:,2)=rad%fbeam(:,3)
        met%fld=-rgsave(cmap)        ! long wave down
