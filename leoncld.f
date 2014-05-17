c9999706f938ab764980b4e7d209685848c93cd0
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2014-05-17 06:19:22 +0000

    Some vectorisation to leoncld.

--- leoncld.f
+++ leoncld.f
@@ -28,18 +28,19 @@
       include 'params.h'
 
 c for cfrp
+      integer kcldfmax(icfrp)
       real taul(icfrp,kl)
       real taui(icfrp,kl)
       real fice(icfrp,kl)
-      integer kcldfmax(icfrp)
-      real tautot,cldmax,ctoptmp,ctoppre
+      real tautot(icfrp),cldmax(icfrp)
+      real ctoptmp(icfrp),ctoppre(icfrp)
       real reffl,tau_sfac,wliq,rk,qlpath,wice,sigmai,cfd,fcf
-      common/leoncfrp/tautot(icfrp),cldmax(icfrp)
-     &               ,ctoptmp(icfrp),ctoppre(icfrp)
 
 c Local variables
       integer iq,k,ncl
-      real rainx,ccw,fl !Stuff for convective cloud fraction
+      real ccw !Stuff for convective cloud fraction
+      real rainx(ifullw)
+      real fl(ifullw)
 
       real prf(ifullw,kl)     !Pressure on full levels (hPa)
       real dprf(ifullw,kl)    !Pressure thickness (hPa)
@@ -88,10 +89,10 @@ c These outputs are not used in this model at present
       nlm=nl-1
 
       do k=1,kl   
-         do iq=1,ifull
-          prf(iq,k)=0.01*ps(iq)*sig(k) !Looks like ps is SI units
-          dprf(iq,k)=-0.01*ps(iq)*dsig(k) !dsig is -ve
-          rhoa(iq,k)=100.*prf(iq,k)/(rdry*t(iq,k))
+        prf(:,k)=0.01*ps(1:ifull)*sig(k) !Looks like ps is SI units
+        dprf(:,k)=-0.01*ps(1:ifull)*dsig(k) !dsig is -ve
+        rhoa(:,k)=100.*prf(:,k)/(rdry*t(1:ifull,k))
+        do iq=1,ifull
           qsg(iq,k)=qsat(100.*prf(iq,k),t(iq,k))
         enddo
       enddo
@@ -119,23 +120,23 @@ c     fluxc(:,:)=rnrt3d(:,:)*1.e-3*dt ! kg/m2 (should be same level as rnrt3d)
 c     Set up convective cloud column
 !     acon=0.2    !Cloud fraction for non-precipitating convection  kuocom.h
 !     bcon=0.07   !Rate at which conv cloud frac increases with R   kuocom.h
-      do iq=1,ifull
-        if(ktsav(iq)<kl-1)then
-          ktop(iq)=ktsav(iq)
-          kbase(iq)=kbsav(iq)+1
-          rainx=condc(iq)*86400./dt !mm/day
-          cldcon(iq)=min(acon + bcon*log(1.0+rainx),0.8) !NCAR
-          wcon(iq)=wlc
-        else
-          cldcon(iq)=0.
-          wcon(iq)=0.
-        endif
-      enddo
+      where ( ktsav<kl-1 )
+        ktop   = ktsav
+        kbase  = kbsav+1
+        rainx  = condc*86400./dt ! mm/day
+        cldcon = min(acon+bcon*log(1.+rainx),0.8) ! NCAR
+        wcon   = wlc
+      elsewhere
+        cldcon = 0.
+        wcon   = 0.
+      end where
 
 !     if(diag.and.mydiag)then
       if(nmaxpr==1.and.mydiag)then
-        if(ktau==1)print *,'in leoncloud acon,bcon,Rcm ',acon,bcon,Rcm
-        print *,'entering leoncld'
+        if(ktau==1) then
+          write(6,*)'in leoncloud acon,bcon,Rcm ',acon,bcon,Rcm
+        end if
+        write(6,*) 'entering leoncld'
         write (6,"('qg  ',3p9f8.3/4x,9f8.3)") qg(idjd,:)
         write (6,"('qf  ',3p9f8.3/4x,9f8.3)") qfg(idjd,:)
         write (6,"('ql  ',3p9f8.3/4x,9f8.3)") qlg(idjd,:)
@@ -143,30 +144,29 @@ c     Set up convective cloud column
 
 c     Calculate convective cloud fraction and adjust moisture variables 
 c     before calling newcloud
-      if (nmr>=1) then ! Max/Rnd cloud overlap
+      if ( nmr>0 ) then ! Max/Rnd cloud overlap
         do k=1,kl
-          where (k<=ktop(1:ifull).and.k>=kbase(1:ifull))
-            clcon(1:ifull,k)=cldcon(1:ifull) ! maximum overlap
+          where ( k<=ktop(1:ifull) .and. k>=kbase(1:ifull) )
+            clcon(:,k)=cldcon(:) ! maximum overlap
             !ccw=wcon(:)/rhoa(:,k)  !In-cloud l.w. mixing ratio
-            qccon(1:ifull,k)=clcon(1:ifull,k)*
-     &                 wcon(1:ifull)/rhoa(1:ifull,k)
-            qcl(1:ifull,k)=max(qsg(1:ifull,k),qg(1:ifull,k))  ! jlm
+            qccon(:,k)=clcon(:,k)*wcon(:)/rhoa(:,k)
+            qcl(:,k)=max(qsg(:,k),qg(1:ifull,k))  ! jlm
             qenv(1:ifull,k)=max(1.e-8,
-     &                 qg(1:ifull,k)-clcon(1:ifull,k)*qcl(1:ifull,k))
-     &                 /(1.-clcon(1:ifull,k))
-            qcl(1:ifull,k)=(qg(1:ifull,k)-(1.-clcon(1:ifull,k))
-     &                *qenv(1:ifull,k))/clcon(1:ifull,k)
-            qlg(1:ifull,k)=qlg(1:ifull,k)/(1.-clcon(1:ifull,k))
-            qfg(1:ifull,k)=qfg(1:ifull,k)/(1.-clcon(1:ifull,k))
+     &                 qg(1:ifull,k)-clcon(:,k)*qcl(:,k))
+     &                 /(1.-clcon(:,k))
+            qcl(:,k)=(qg(1:ifull,k)-(1.-clcon(:,k))
+     &                *qenv(1:ifull,k))/clcon(:,k)
+            qlg(1:ifull,k)=qlg(1:ifull,k)/(1.-clcon(:,k))
+            qfg(1:ifull,k)=qfg(1:ifull,k)/(1.-clcon(:,k))
           elsewhere
-            clcon(1:ifull,k)=0.
-            qccon(1:ifull,k)=0.
-            qcl(1:ifull,k)=0.
-            qenv(1:ifull,k)=qg(1:ifull,k)
+            clcon(:,k)      = 0.
+            qccon(:,k)      = 0.
+            qcl(1:ifull,k)  = 0.
+            qenv(1:ifull,k) = qg(1:ifull,k)
           endwhere
-        enddo
+        end do
+        
       else ! usual random cloud overlap
-      
         do k=1,kl
           do iq=1,ifull
             if(k<=ktop(iq).and.k>=kbase(iq))then
@@ -197,8 +197,8 @@ c     before calling newcloud
       
       tenv(:,:)=t(1:ifull,:) !Assume T is the same in and out of convective cloud
 
-      if(nmaxpr==1.and.mydiag)then
-        print *,'before newcloud',ktau
+      if ( nmaxpr==1 .and. mydiag ) then
+        write(6,*) 'before newcloud',ktau
         write (6,"('t   ',9f8.2/4x,9f8.2)") t(idjd,:)
         write (6,"('qg  ',3p9f8.3/4x,9f8.3)") qg(idjd,:)
         write (6,"('qf  ',3p9f8.3/4x,9f8.3)") qfg(idjd,:)
@@ -207,16 +207,17 @@ c     before calling newcloud
         write (6,"('qsg ',3p9f8.3/4x,9f8.3)") qsg(idjd,:)
         write (6,"('qcl ',3p9f8.3/4x,9f8.3)") qcl(idjd,:)
         write (6,"('clc ',9f8.3/4x,9f8.3)") clcon(idjd,:)
-        print *,'cldcon,kbase,ktop ',cldcon(idjd),kbase(idjd),ktop(idjd)
+        write(6,*) 'cldcon,kbase,ktop ',cldcon(idjd),kbase(idjd),
+     &    ktop(idjd)
       endif
 
 c     Calculate cloud fraction and cloud water mixing ratios
       call newcloud(dt,land,prf,kbase,ktop,rhoa,cdso4, !Inputs
-     &     tenv,qenv,qlg,qfg,   !In and out  t here is tenv
-     &     cfrac,ccov,cfa,qca)      !Outputs
+     &     tenv,qenv,qlg,qfg,                          !In and out  t here is tenv
+     &     cfrac,ccov,cfa,qca)                         !Outputs
 
       if(nmaxpr==1.and.mydiag)then
-        print *,'after newcloud',ktau
+        write(6,*) 'after newcloud',ktau
         write (6,"('tnv ',9f8.2/4x,9f8.2)") tenv(idjd,:)
         write (6,"('qg0 ',3p9f8.3/4x,9f8.3)") qg(idjd,:)
         write (6,"('qf  ',3p9f8.3/4x,9f8.3)") qfg(idjd,:)
@@ -226,22 +227,20 @@ c     Calculate cloud fraction and cloud water mixing ratios
 
 c     Weight output variables according to non-convective fraction of grid-box            
       do k=1,kl
-        do iq=1,ifull
-          t(iq,k)=clcon(iq,k)*t(iq,k)+(1.-clcon(iq,k))*tenv(iq,k)
-          qg(iq,k)=clcon(iq,k)*qcl(iq,k)+(1.-clcon(iq,k))*qenv(iq,k)
-          if(k>=kbase(iq).and.k<=ktop(iq))then
-            cfrac(iq,k)=cfrac(iq,k)*(1.-clcon(iq,k))
-            ccov(iq,k)=ccov(iq,k)*(1.-clcon(iq,k))              
-            qlg(iq,k)=qlg(iq,k)*(1.-clcon(iq,k))
-            qfg(iq,k)=qfg(iq,k)*(1.-clcon(iq,k))
-            cfa(iq,k)=cfa(iq,k)*(1.-clcon(iq,k))
-            qca(iq,k)=qca(iq,k)*(1.-clcon(iq,k))              
-          endif
-        enddo
+        t(1:ifull,k)=clcon(:,k)*t(1:ifull,k)+(1.-clcon(:,k))*tenv(:,k)
+        qg(1:ifull,k)=clcon(:,k)*qcl(:,k)+(1.-clcon(:,k))*qenv(:,k)
+        where ( k>=kbase .and. k<=ktop )
+          cfrac(:,k)=cfrac(:,k)*(1.-clcon(:,k))
+          ccov(:,k)=ccov(:,k)*(1.-clcon(:,k))              
+          qlg(1:ifull,k)=qlg(1:ifull,k)*(1.-clcon(:,k))
+          qfg(1:ifull,k)=qfg(1:ifull,k)*(1.-clcon(:,k))
+          cfa(:,k)=cfa(:,k)*(1.-clcon(:,k))
+          qca(:,k)=qca(:,k)*(1.-clcon(:,k))              
+        end where
       enddo
 
       if(nmaxpr==1.and.mydiag)then
-        print *,'before newrain',ktau
+        write(6,*) 'before newrain',ktau
         write (6,"('t   ',9f8.2/4x,9f8.2)") t(idjd,:)
         write (6,"('qg  ',3p9f8.3/4x,9f8.3)") qg(idjd,:)
         write (6,"('qf  ',3p9f8.3/4x,9f8.3)") qfg(idjd,:)
@@ -260,12 +259,10 @@ c     Weight output variables according to non-convective fraction of grid-box
 !     done because sometimes newrain drops out all qlg, ending up with 
 !     zero cloud (although it will be rediagnosed as 1 next timestep)
       do k=1,kl
-        do iq=1,ifull
-c         cfrac(iq,k)=min(1.,ccov(iq,k)+clcon(iq,k))
-          fl=max(0.0,min(1.0,(t(iq,k)-ticon)/(273.15-ticon)))
-          qlrad(iq,k)=qlg(iq,k)+fl*qccon(iq,k)
-          qfrad(iq,k)=qfg(iq,k)+(1.-fl)*qccon(iq,k)
-        enddo
+c       cfrac(iq,k)=min(1.,ccov(iq,k)+clcon(iq,k))
+        fl=max(0.,min(1.,(t(1:ifull,k)-ticon)/(273.15-ticon)))
+        qlrad(:,k)=qlg(:,k)+fl*qccon(:,k)
+        qfrad(:,k)=qfg(:,k)+(1.-fl)*qccon(:,k)
       enddo
 
 c     Calculate precipitation and related processes
@@ -276,7 +273,7 @@ c     Calculate precipitation and related processes
      &    preci,qevap,qsubl,qauto,qcoll,qaccr,fluxr,fluxi,  !Outputs
      &    fluxm,pfstay,pqfsed,slopes,prscav)                !Outputs
       if(nmaxpr==1.and.mydiag)then
-        print *,'after newrain',ktau
+        write(6,*) 'after newrain',ktau
         write (6,"('t   ',9f8.2/4x,9f8.2)") t(idjd,:)
         write (6,"('qg  ',3p9f8.3/4x,9f8.3)") qg(idjd,:)
         write (6,"('qf  ',3p9f8.3/4x,9f8.3)") qfg(idjd,:)
@@ -326,92 +323,90 @@ c     Calculate precipitation and related processes
 !     done because sometimes newrain drops out all qlg, ending up with 
 !     zero cloud (although it will be rediagnosed as 1 next timestep)
       do k=1,kl
-        do iq=1,ifull
-          cfrac(iq,k)=min(1.,ccov(iq,k)+clcon(iq,k))
-c          fl=max(0.0,min(1.0,(t(iq,k)-ticon)/(273.15-ticon)))
-c          qlrad(iq,k)=qlg(iq,k)+fl*qccon(iq,k)
-c          qfrad(iq,k)=qfg(iq,k)+(1.-fl)*qccon(iq,k)
-        enddo
+        cfrac(:,k)=min(1.,ccov(:,k)+clcon(:,k))
+c       fl=max(0.0,min(1.0,(t(iq,k)-ticon)/(273.15-ticon)))
+c       qlrad(iq,k)=qlg(iq,k)+fl*qccon(iq,k)
+c       qfrad(iq,k)=qfg(iq,k)+(1.-fl)*qccon(iq,k)
       enddo
 
 !========================= Jack's diag stuff =========================
-       if(ncfrp==1)then  ! from here to near end; Jack's diag stuff
-        do iq=1,icfrp
-          tautot(iq)=0.
-          cldmax(iq)=0.
-          ctoptmp(iq)=0.
-          ctoppre(iq)=0.
-          do k=1,kl
-            fice(iq,k)=0.
-          enddo
-          kcldfmax(iq)=0.
-        enddo
-c       cfrp data
-        do k=1,kl-1
-          do iq=1,icfrp
-            taul(iq,k)=0.
-            taui(iq,k)=0.
-            Reffl=0.
-            if(cfrac(iq,k)>0.)then
-              tau_sfac=1.
-c             fice(iq,k) = qfg(iq,k)/(qfg(iq,k)+qlg(iq,k))
-              fice(iq,k) = qfrad(iq,k)/(qfrad(iq,k)+qlrad(iq,k)) ! 16/1/06
-              !rhoa=100.*prf(iq,k)/(rdry*t(iq,k))
-              !dpdp=dprf(iq,k)/prf(iq,k)
-              !dz=dpdp*rdry*t(iq,k)/grav
-c             Liquid water clouds
-              if(qlg(iq,k)>1.0e-8)then
-                Wliq=rhoa(iq,k)*qlg(iq,k)/(cfrac(iq,k)*(1-fice(iq,k))) !kg/m^3
-                if(.not.land(iq))then !sea
-                  rk=0.8
-                else            !land
-                  rk=0.67
-                endif
+      if(ncfrp==1)then  ! from here to near end; Jack's diag stuff
+       do iq=1,icfrp
+         tautot(iq)=0.
+         cldmax(iq)=0.
+         ctoptmp(iq)=0.
+         ctoppre(iq)=0.
+         do k=1,kl
+           fice(iq,k)=0.
+         enddo
+         kcldfmax(iq)=0.
+       enddo
+c      cfrp data
+       do k=1,kl-1
+         do iq=1,icfrp
+           taul(iq,k)=0.
+           taui(iq,k)=0.
+           Reffl=0.
+           if(cfrac(iq,k)>0.)then
+             tau_sfac=1.
+c            fice(iq,k) = qfg(iq,k)/(qfg(iq,k)+qlg(iq,k))
+             fice(iq,k) = qfrad(iq,k)/(qfrad(iq,k)+qlrad(iq,k)) ! 16/1/06
+             !rhoa=100.*prf(iq,k)/(rdry*t(iq,k))
+             !dpdp=dprf(iq,k)/prf(iq,k)
+             !dz=dpdp*rdry*t(iq,k)/grav
+c            Liquid water clouds
+             if(qlg(iq,k)>1.0e-8)then
+               Wliq=rhoa(iq,k)*qlg(iq,k)/(cfrac(iq,k)*(1-fice(iq,k))) !kg/m^3
+               if(.not.land(iq))then !sea
+                 rk=0.8
+               else            !land
+                 rk=0.67
+               endif
 c Reffl is the effective radius at the top of the cloud (calculated following
 c Martin etal 1994, JAS 51, 1823-1842) due to the extra factor of 2 in the
 c formula for reffl. Use mid cloud value of Reff for emissivity.
-                Reffl=(3*2*Wliq/(4*rhow*pi*rk*cdso4(iq,k)))**(1./3)
-                qlpath=Wliq*dz(iq,k)
-                taul(iq,k)=tau_sfac*1.5*qlpath/(rhow*Reffl)
-              endif ! qlg
+               Reffl=(3*2*Wliq/(4*rhow*pi*rk*cdso4(iq,k)))**(1./3)
+               qlpath=Wliq*dz(iq,k)
+               taul(iq,k)=tau_sfac*1.5*qlpath/(rhow*Reffl)
+             endif ! qlg
 c Ice clouds
-              if(qfg(iq,k)>1.0e-8)then
-                Wice=rhoa(iq,k)*qfg(iq,k)/(cfrac(iq,k)*fice(iq,k)) !kg/m**3
-                sigmai = aice*Wice**bice !visible ext. coeff. for ice
-                taui(iq,k)=sigmai*dz(iq,k) !visible opt. depth for ice
-                taui(iq,k)=tau_sfac*taui(iq,k)
-              endif ! qfg
-            endif !cfrac
-          enddo ! iq
-        enddo ! k
+             if(qfg(iq,k)>1.0e-8)then
+               Wice=rhoa(iq,k)*qfg(iq,k)/(cfrac(iq,k)*fice(iq,k)) !kg/m**3
+               sigmai = aice*Wice**bice !visible ext. coeff. for ice
+               taui(iq,k)=sigmai*dz(iq,k) !visible opt. depth for ice
+               taui(iq,k)=tau_sfac*taui(iq,k)
+             endif ! qfg
+           endif !cfrac
+         enddo ! iq
+       enddo ! k
 c Code to get vertically integrated value...
 c top down to get highest level with cfrac=cldmax (kcldfmax)
 !        do k=kl,1,-1
 !       Need to start at kl-1 to work with NaN initialisation.
-        do k=kl-1,1,-1
-          do iq=1,icfrp
-            tautot(iq)=tautot(iq)+cfrac(iq,k)*(fice(iq,k)
-     &                *taui(iq,k)+(1.-fice(iq,k))*taul(iq,k))
-            if(cfrac(iq,k)>cldmax(iq)) kcldfmax(iq)=k
-            cldmax(iq)=max(cldmax(iq),cfrac(iq,k))
-          enddo ! iq
-        enddo ! k
+       do k=kl-1,1,-1
+         do iq=1,icfrp
+           tautot(iq)=tautot(iq)+cfrac(iq,k)*(fice(iq,k)
+     &               *taui(iq,k)+(1.-fice(iq,k))*taul(iq,k))
+           if(cfrac(iq,k)>cldmax(iq)) kcldfmax(iq)=k
+           cldmax(iq)=max(cldmax(iq),cfrac(iq,k))
+         enddo ! iq
+       enddo ! k
 
-        do iq=1,icfrp
-          if(cldmax(iq)>1.e-10) then
-            tautot(iq)=tautot(iq)/cldmax(iq)
+       do iq=1,icfrp
+         if(cldmax(iq)>1.e-10) then
+           tautot(iq)=tautot(iq)/cldmax(iq)
 
-            cfd=0.
-            do k=kl,kcldfmax(iq),-1
-              fcf = max(0.,cfrac(iq,k)-cfd) ! cld frac. from above
-              ctoptmp(iq)=ctoptmp(iq)+fcf*t(iq,k)/cldmax(iq)
-              ctoppre(iq)=ctoppre(iq)+fcf*prf(iq,k)/cldmax(iq)
-              cfd=max(cfrac(iq,k),cfd)
-            enddo ! k=kl,kcldfmax(iq),-1
+           cfd=0.
+           do k=kl,kcldfmax(iq),-1
+             fcf = max(0.,cfrac(iq,k)-cfd) ! cld frac. from above
+             ctoptmp(iq)=ctoptmp(iq)+fcf*t(iq,k)/cldmax(iq)
+             ctoppre(iq)=ctoppre(iq)+fcf*prf(iq,k)/cldmax(iq)
+             cfd=max(cfrac(iq,k),cfd)
+           enddo ! k=kl,kcldfmax(iq),-1
 
-          endif ! (cldmax(iq).gt.1.e-10) then
-        enddo   ! iq
-       endif    ! ncfrp.eq.1
+         endif ! (cldmax(iq).gt.1.e-10) then
+       enddo   ! iq
+      endif    ! ncfrp.eq.1
 !========================= end of Jack's diag stuff ======================
 
 !     factor of 2 is because used .5 in newrain.f (24-mar-2000, jjk)
