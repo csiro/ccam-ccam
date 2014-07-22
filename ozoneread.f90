2a90562e336f262c2b5ca78ecf076173c0a4a9e4
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2014-07-22 23:07:53 +0000

    Consolidate ozone read routines into a common module.

--- ozoneread.f90
+++ ozoneread.f90
@@ -9,13 +9,23 @@ implicit none
 
 private      
 public o3_read,o3set,fieldinterpolate,o3regrid
-      
+public o3read_amip,o3set_amip
+
+! CMIP3 / CMIP5 ozone fields
 integer, save :: ii,jj,kk
 real, dimension(:,:), allocatable, save :: o3pre,o3mth,o3nxt
 real, dimension(:), allocatable, save :: o3pres
 real, dimension(:,:), allocatable, save :: dduo3n,ddo3n2
 real, dimension(:,:), allocatable, save :: ddo3n3,ddo3n4
-      
+
+! o3amip parameters
+integer, parameter :: mo=12    ! Months
+integer, parameter :: jg=64    ! Latitudes
+integer, parameter :: kg=59    ! Levels
+integer, parameter :: lg=kg+1  ! Layer Interfaces
+real, dimension(:), allocatable, save :: glat,dp,gpri
+real, dimension(:,:,:), allocatable, save :: gdat
+
 contains
 
 !--------------------------------------------------------------
@@ -535,5 +545,212 @@ end do
 
 return
 end subroutine resetd
+
+subroutine o3read_amip
+
+!  Read the AMIP2 lat-height ozone data (volume mixing ratio)
+!  Based on the supplied example program to read the data.
+!  This routine is fixed format f90
+
+use cc_mpi
+
+implicit none
+      
+include 'filnames.h'
+
+character(len=120) :: label
+real, parameter :: amd = 28.9644, amo = 48.0000
+real, parameter :: massratio = amo / amd
+integer :: lato3d  ! = jg  number of data latitudinal grid
+integer :: layo3d  ! = kg  number of data vertical layers
+integer :: lvlo3d  ! = lg  number of data vertical layer interfaces
+integer :: j, k, month, ierr
+integer, parameter :: un = 16  ! Unit number for file
+real, dimension(kg) :: galt, gprs
+real, dimension(lg) :: gali
+real :: o3vubc  ! =     upper boundary o3 vmr (ppmv)
+real :: prsubc  ! =     upper boundary pressure (mb)
+real :: altubc  ! =     upper boundary altitude (km)
+
+allocate(glat(jg),dp(kg),gpri(lg),gdat(jg,kg,mo))
+
+if (myid==0) then
+
+  open(un,file=o3file,form='formatted',status='old')
+  read (un,"(i3)") lato3d
+  read (un,"(i3)") layo3d
+  lvlo3d  =    1 + layo3d
+  if ( lato3d /= jg ) then
+    write(6,*) "Error in horizontal resolution of ozone data"
+    call ccmpi_abort(-1)
+  end if
+  if ( layo3d /= kg ) then
+    write(6,*) "Error in vertical resolution of ozone data"
+    call ccmpi_abort(-1)
+  end if
+
+  read (un,"(3(1pe12.5))") o3vubc, prsubc, altubc
+  read (un,"(a)") label   ! grid latitudes (deg) --------->
+  read (un,"(10(1pe12.5))") (glat(j),j=1,lato3d)
+  read (un,"(a)") label   ! layer pressure (mb) ---------->
+  read (un,"(10(1pe12.5))") (gprs(k),k=1,layo3d)
+  read (un,"(a)") label   ! layer altitude (km) ---------->
+  read (un,"(10(1pe12.5))") (galt(k),k=1,layo3d)
+  read (un,"(a)") label   ! interface pressure (mb) ------>
+  read (un,"(10(1pe12.5))") (gpri(k),k=1,lvlo3d)
+  read (un,"(a)") label   ! interface altitude (km) ------>
+  read (un,"(10(1pe12.5))") (gali(k),k=1,lvlo3d)
+  do month = 1,mo
+    read (un,"(a)") label   ! monthly o3 vmr (ppmv) lat-alt
+    read (un,"(10(1pe12.5))") ((gdat(j,k,month),j=1,lato3d),k=1,layo3d)
+  enddo
+  close(un)
+end if
+
+call ccmpi_bcast(glat,0,comm_world)
+call ccmpi_bcast(gpri,0,comm_world)
+call ccmpi_bcast(gdat,0,comm_world)
+
+dp = gpri(2:lg) - gpri(1:lg-1)
+
+! Convert from ppmv to mass mixing ratio
+gdat = gdat * 1.e-6 * massratio
+
+return
+end subroutine o3read_amip
+
+subroutine o3set_amip ( alat, npts, mins, sigh, ps, qo3 )
+
+! Interpolate the AMIP2 ozone data in time, latitude and pressure to 
+! produce ozone mixing ratio for model radiation code. 
+
+! In CCAM version latitude may be different for every point.
+
+implicit none
+
+include 'newmpar.h'
+include 'const_phys.h'
+include 'dates.h'
+
+integer, intent(in) :: npts
+real, dimension(npts), intent(in) :: alat     ! Latitude
+integer, intent(in) :: mins  ! Time
+real, intent(in),  dimension(kl+1) :: sigh ! Half level sigma
+real, intent(in),  dimension(npts) :: ps  ! Surface pressure
+real, intent(out), dimension(npts,kl) :: qo3  ! Ozone mixing ratio
+! Day no of middle of month
+real, dimension(12) :: monmid
+real, parameter, dimension(12) :: oldmid = (/ 15.5, 45.0, 74.5, 105.0, 135.5, 166.0, 196.5, 227.5, 258.0, 288.5, 319.0, 349.5 /)
+real, dimension(kg) :: oz    ! Column for this date and lat.
+real, dimension(lg) :: ozcol ! Integrated column
+real, dimension(kl+1) :: qo3p
+integer :: k1, jyear
+integer :: j, m1, m2, j1, j2, k, kk, k1min
+real, dimension(kl+1) :: prh ! Half level pressure
+real :: fac1, fac2, tfac1, tfac2, date, theta
+
+integer leap
+common/leap_yr/leap  ! 1 to allow leap years
+
+monmid=oldmid
+if ( leap==1 ) then
+  jyear =kdate/10000
+  if ( mod(jyear,4)==0 ) then
+    monmid(2)=oldmid(2)+0.5
+    monmid(3:12)=oldmid(3:12)+1.
+  end if
+  if ( mod(jyear,100)==0 ) then
+    monmid(2:12)=oldmid(2:12)
+  end if
+  if ( mod(jyear,400)==0 ) then
+    monmid(2)=oldmid(2)+0.5
+    monmid(3:12)=oldmid(3:12)+1.
+  end if
+end if
+
+! Time interpolation factors
+date = real(modulo(mins,nint(monmid(12)+15.5)*1440)) / 1440.0
+if ( date <= monmid(1) ) then ! First half of Jan
+  m1 = 12
+  m2 = 1
+  tfac1 = (monmid(1) - date)/31.0
+  tfac2 = 1.0 - tfac1
+else if ( date >= monmid(12) ) then
+  m1 = 12
+  m2 = 1
+  tfac2 = (date - monmid(12))/31.0
+  tfac1 = 1 - tfac2
+else
+! Search for bracketing dates, i such that monmid(i) >= date
+  do m2=2,12
+    if ( monmid(m2) >= date ) exit
+  end do
+  m1 = m2 - 1
+  tfac1 = ( monmid(m2) - date ) / ( monmid(m2) - monmid(m1) )
+  tfac2 = 1.0 - tfac1
+end if
+
+do j=1,npts
+! Factors for interpolation in latitude, 
+! Note that ozone grid latitudes run S to N.
+  theta = alat(j)*180.0/pi
+  if ( theta <= glat(1) ) then
+    j1 = 1
+    j2 = 1
+    fac1 = 1.0
+    fac2 = 0.0
+  else if ( theta >= glat(jg) ) then
+    j1 = jg
+    j2 = jg
+    fac1 = 1.0
+    fac2 = 0.0
+  else
+!   Input data isn't exactly equally spaced, so search to find the spanning
+!   latitudes. Want to find first j such that glat(j) >= theta
+    j2 = 1 + count ( glat < theta )
+    j1 = j2 - 1
+    fac1 = ( glat(j2) - theta ) / ( glat(j2) - glat(j1) )
+    fac2 = 1.0 - fac1
+  end if
+
+! Now interpolate in latitude and time.
+  oz = tfac1 * ( fac1*gdat(j1,:,m1) + fac2*gdat(j2,:,m1 ) ) + tfac2 * ( fac1*gdat(j1,:,m2) + fac2*gdat(j2,:,m2 ) ) 
+
+! Each longitude has same vertical profile as a function of pressure,
+! but model levels depend on surface pressure so each must be 
+! interpolated separately. To ensure the column amount is maintained
+! correctly, first calculate ozone column between every half level and
+! TOA.
+  ozcol(1) = 0.0
+  do k=1,kg
+    ozcol(k+1) = ozcol(k) + oz(k)*dp(k)
+  end do
+
+! To calculate model amounts, find the data half level immediately above
+! the given model half level. Note that model levels start at surface while
+! data levels start at the top.
+!
+  prh = sigh * ps(j)*0.01 ! Input PS in Pa
+
+  qo3p = 0.
+  qo3p(kl+1) = 0.0      ! TOA
+  do k=kl,1,-1          ! Start at the top model level
+
+!   Find largest k1 such that gpri(k1) <= prh(k)
+!   Restrict to range 1:kg so as not to go out of bounds of oz
+    k1 = max(1, count( gpri(1:kg) <= prh(k) ) )
+    qo3p(k) = ozcol(k1) + (prh(k)-gpri(k1))*oz(k1)
+  end do
+
+! Finally get the model ozone mixing ratios by differencing the half level
+! path amounts. The vertical order of the qo3 array is reversed to 
+! match the radiation code.
+  do k=1,kl
+    qo3(j,kl+1-k) = ( qo3p(k+1) - qo3p(k) ) / ( prh(k+1) - prh(k) )
+  end do
+end do
+
+return
+end subroutine o3set_amip
 
 end module ozoneread
