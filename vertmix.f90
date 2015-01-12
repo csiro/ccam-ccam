1b82c1616d8dafd549a67a92c949d41c3743777d
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2015-01-12 07:48:58 +0000

    Bug fix for tke-eps interface

--- vertmix.f90
+++ vertmix.f90
@@ -366,10 +366,10 @@ else
   ! Evaluate EDMF scheme
   select case(nlocal)
     case(0) ! no counter gradient
-      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,ustar,zo,zg,zh,sig,rhos,dt,qgmin,1,0,tnaero,xtg)
+      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,zo,zg,zh,sig,rhos,dt,qgmin,1,0,tnaero,xtg)
       rkh=rkm
     case(1,2,3,4,5,6) ! KCN counter gradient method
-      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,ustar,zo,zg,zh,sig,rhos,dt,qgmin,1,0,tnaero,xtg)
+      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,zo,zg,zh,sig,rhos,dt,qgmin,1,0,tnaero,xtg)
       rkh=rkm
       do k=1,kl
         uav(1:ifull,k)=av_vmod*u(1:ifull,k)+(1.-av_vmod)*(savu(1:ifull,k)-ou)
@@ -377,7 +377,7 @@ else
       end do
       call pbldif(rhs,rkh,rkm,uav,vav)
     case(7) ! mass-flux counter gradient
-      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,ustar,cduv,zg,zh,sig,rhos,dt,qgmin,0,0,tnaero,xtg)
+      call tkemix(rkm,rhs,qg,qlg,qfg,qrg,cldtmp,rfrac,u,v,pblh,fg,eg,ps,zo,zg,zh,sig,rhos,dt,qgmin,0,0,tnaero,xtg)
       rkh=rkm
     case DEFAULT
       write(6,*) "ERROR: Unknown nlocal option for nvmix=6"
