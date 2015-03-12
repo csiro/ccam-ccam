fc32741397f2541be3c114bce11d557e71a8f125
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2015-03-12 23:38:15 +0000

    Clean-up small sea-ice in 1D ice model

--- mlo.f90
+++ mlo.f90
@@ -1151,6 +1151,7 @@ if (calcprog) then
   ice%store=d_nsto
   call mloice(dt,atm_ps,d_alpha,d_beta,d_b0,d_wu0,d_wv0,d_wt0,d_ws0,d_ftop,d_tb,d_fb,d_timelt,       &
               d_ustar,d_nk,d_neta,d_zcr,diag)
+  call mlonewice(dt,d_timelt,d_zcr,diag)
 
   ! update water
   call mlocalc(dt,atm_f,d_rho,d_nsq,d_rad,d_alpha,d_beta,d_b0,d_ustar,d_wu0,d_wv0,d_wt0,d_ws0,      &
