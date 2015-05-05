3d214d1680edb387f989780627e42b97830f4ac6
Thatcher, Marcus (O&A, Aspendale) <Marcus.Thatcher@csiro.au>
2015-05-05 20:02:36 +0000

    More aerosol diagnostic printouts for SEA-ESF radiation

--- seaesfrad.f90
+++ seaesfrad.f90
@@ -548,6 +548,19 @@ end if
 if ( diag .and. mydiag ) then
   write(6,*) "tdiag ",t(idjd,:)
   write(6,*) "qgdiag ",qg(idjd,:)
+  write(6,*) "qlgraddiag ",qlrad(idjd,:)
+  write(6,*) "qfgraddiag ",qfrad(idjd,:)
+  write(6,*) "SO4diag ",xtg(idjd,:,3)
+  write(6,*) "BCphobdiag ",xtg(idjd,:,4)
+  write(6,*) "BCphildiag ",xtg(idjd,:,5)
+  write(6,*) "OCphobdiag ",xtg(idjd,:,6)
+  write(6,*) "OCphildiag ",xtg(idjd,:,7)
+  write(6,*) "dust0.8diag ",xtg(idjd,:,8)
+  write(6,*) "dust1.0diag ",xtg(idjd,:,9)
+  write(6,*) "dust2.0diag ",xtg(idjd,:,10)
+  write(6,*) "dust4.0diag ",xtg(idjd,:,11)
+  write(6,*) "Saltfilmdiag ",ssn(idjd,:,1)
+  write(6,*) "Saltjetdiag  ",ssn(idjd,:,2)
 end if
 
 ! error checking
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-      character(len=*), parameter :: version ='CCAM r2062:2063M'
+      character(len=*), parameter :: version ='CCAM r2064'
