41c7417e114507dd62f434cfc99c9d8a925f29f4
rya17a <rya17a@localhost>
2015-11-18 22:50:50 +0000

    Change diskless to netcdf4 for file under /tmp

--- infile.f90
+++ infile.f90
@@ -1788,7 +1788,7 @@ if ( procformat ) then
       !On Magnus ior(nf90_netcdf4,nf90_diskless) doesn't work use nf90_diskless only
       !but seems to be 32bit limited - workaround open real file on /tmp
       !ncstatus = nf90_create(fname,nf90_diskless,lncid)
-      ncstatus = nf90_create("/tmp/"//trim(fname),nf90_diskless,lncid)
+      ncstatus = nf90_create("/tmp/"//trim(fname),nf90_netcdf4,lncid)
    end if
 else
    ncstatus = nf90_create(fname,mode,lncid)
