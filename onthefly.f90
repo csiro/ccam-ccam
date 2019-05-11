573cd314e37218c57f761715bf30db74979da6b5
tha051 <tha051@localhost>
2019-05-11 21:41:44 +0000

    Bug fix for checking thetao in onthefly

--- onthefly.f90
+++ onthefly.f90
@@ -1072,12 +1072,6 @@ if ( abs(nmlo)>=1 .and. abs(nmlo)<=9 .and. nested/=3 ) then
       where ( mlodwn(1:ifull,1:wlev,1)>100. )
         mlodwn(1:ifull,1:wlev,1) = mlodwn(1:ifull,1:wlev,1) - wrtemp ! remove temperature offset for precision
       end where
-      if ( any((mlodwn(1:ifull,1:wlev,1)+wrtemp)<100.) .or. any((mlodwn(1:ifull,1:wlev,1)+wrtemp)>400.) ) then
-        write(6,*) "ERROR: ocean potental temperature is out-of-range in onthefly"
-        write(6,*) "minval,maxval ",minval(mlodwn(1:ifull,1:wlev,1)+wrtemp),maxval(mlodwn(1:ifull,1:wlev,1)+wrtemp)
-        write(6,*) "minloc,maxloc ",minloc(mlodwn(1:ifull,1:wlev,1)+wrtemp),maxloc(mlodwn(1:ifull,1:wlev,1)+wrtemp)
-        call ccmpi_abort(-1)
-      end if
     end if ! (nestesd/=1.or.nud_sst/=0) ..else..
     ! ocean salinity
     if ( (nested/=1.or.nud_sss/=0) .and. ok>0 ) then
