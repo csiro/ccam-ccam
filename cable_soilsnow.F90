f343582b2f198975b2f4f9a9c3932b484921b2d2
tha051 <tha051@localhost>
2020-07-20 07:28:41 +0000

    Bug fix for cable_soilsnow

--- cable_soilsnow.F90
+++ cable_soilsnow.F90
@@ -533,13 +533,14 @@ CONTAINS
 
     TYPE(soil_parameter_type), INTENT(INOUT) :: soil
 
-    INTEGER, DIMENSION(mp,3) :: ssnow_isflag_ssdn
+    !INTEGER, DIMENSION(mp,3) :: ssnow_isflag_ssdn ! MJT suggestion
     REAL, DIMENSION(mp) :: ssnow_tgg_min1
-    REAL, DIMENSION(mp,3) :: dels_ssdn, ssnow_tgg_min
+    !REAL, DIMENSION(mp,3) :: dels_ssdn, ssnow_tgg_min ! MJT suggestion
 
-    ssnow_isflag_ssdn = SPREAD( ssnow%isflag,2,mp)
+    !MJT suggestion
+    !ssnow_isflag_ssdn = SPREAD( ssnow%isflag,2,mp)
 
-    dels_ssdn = SPREAD( SPREAD( dels, 1, mp ), 2,  mp )
+    !dels_ssdn = SPREAD( SPREAD( dels, 1, mp ), 2,  mp )
     ssnow_tgg_min1 = MIN( C%TFRZ, ssnow%tgg(:,1) )
 
     WHERE( ssnow%snowd > 0.1 .AND. ssnow%isflag == 0 )
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-      character(len=*), parameter :: version ='CCAM r4752:4753M'
+      character(len=*), parameter :: version ='CCAM r4752:4754M'
