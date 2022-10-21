1853e15e9f37ac847d2353159514bc2458aed873
Thatcher, Marcus (O&A Aspendale) <Marcus.Thatcher@csiro.au>
2022-10-21 14:58:18 +1100

    Fix for SW radiation crash due to divide by zero

--- esfsw_driver.f90
+++ esfsw_driver.f90
@@ -5144,9 +5144,9 @@ logical, dimension(:,:,:), intent(in), optional    :: cloud
 !    notes: the number of streams for the diffuse beam is fixed at 4.   
 !    this calculation is done only for ng=1.                 
 !----------------------------------------------------------------------c
- 
-                      tt1 = 0.75 * ww1            / ( 1.0 - ( qq4 * &
-                              cosangstr(1) ) ** 2 )
+                      tmp = 1.0 - ( qq4 * cosangstr(1) ) ** 2
+                      tmp = sign(1.,tmp)*max(abs(tmp),1.e-20) ! MJT suggestion
+                      tt1 = 0.75 * ww1 / tmp
                       tt2 = tt1 * cosangstr(1) * ( 1.0 +  &
                               ww2        * qq1 * onedi3 )
                       tt3 = tt1 * ( 1.0 + ww2        * qq1*&
