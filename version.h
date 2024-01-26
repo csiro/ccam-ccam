a0c79cb621a53f51195bbfa456e6e87a39395489
Thatcher, Marcus (O&A Aspendale) <Marcus.Thatcher@csiro.au>
2024-01-26 22:12:50 +1100

    Include ni with ints update

--- version.h
+++ version.h
@@ -1,2 +1,2 @@
 character(len=*), parameter :: version= &
-'CCAM Date:   Thu Jan 25 19:42:35 2024 +1100 commit ada0890b16ca070f015b812ff11cc248d819bee3' 
+'CCAM Date:   Fri Jan 26 19:10:32 2024 +1100 commit ce845da517a38e54db1b63918f4e4b7f2f1b9810' 
